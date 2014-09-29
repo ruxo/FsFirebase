a18b6b38db4f2833c783ed98c7f98fcf2236e7b5
Samrit Taechawongtham <samrit.taechawongtham@thomsonreuters.com>
2014-09-29 19:07:56 +0700

    First version

---
+++ FsFirebase/FsFirebase.fs
@@ -0,0 +1,21 @@
+﻿module FsFirebase
+
+open System.Net.Http
+open Newtonsoft.Json
+
+[<RequireQualifiedAccess>]
+module Json =
+    let fromObj o = JsonConvert.SerializeObject (o:obj)
+
+
+let putAsync(url:string, data) =
+    let json = new StringContent(data)
+
+    async {
+        use client = new HttpClient()
+        let! response = Async.AwaitTask <| client.PutAsync(url, json)
+        let! content = Async.AwaitTask <| response.Content.ReadAsStringAsync()
+        return match response.IsSuccessStatusCode with
+               | true -> Choice1Of2 content
+               | false -> Choice2Of2 (int response.StatusCode, content)
+    }
---
+++ FsFirebase/Script.fsx
@@ -0,0 +1,16 @@
+﻿#r "System.Net.Http.dll"
+#r "Newtonsoft.json.dll"
+
+#load "FsFirebase.fs"
+open FsFirebase
+
+printfn "Start!!"
+
+let url = "https://a7knbwy6th8.firebaseio-demo.com/user.json"
+let data = """{ "someone": { "name": "Alan Turing", "birthday": "June 23, 1912" } }"""
+async {
+    let! result = putAsync(url, data)
+    match result with
+    | Choice1Of2 r -> printfn "Response = %s" r
+    | Choice2Of2 (code, msg) -> printfn "Put failed with %d: %s" code msg
+} |> Async.RunSynchronously
