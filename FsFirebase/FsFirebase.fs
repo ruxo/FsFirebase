06cf8f7845df8ce3ba4cc47255a094ef72114255
Ruxoz <github@just.sent.at>
2014-10-07 12:56:56 +0700

    Finish Json.fromKeyPairs, forming a string from JSON record.

--- FsFirebase/FsFirebase.fs
+++ FsFirebase/FsFirebase.fs
@@ -3,10 +3,39 @@
 open System.Net.Http
 open Newtonsoft.Json
 
+type JSON =
+    | JString of string
+    | JNumber of decimal
+    | JArray of JSON list
+    | JObject of (string * JSON) list option
+
 [<RequireQualifiedAccess>]
 module Json =
-    let fromObj o = JsonConvert.SerializeObject (o:obj)
+    open System
+    open Newtonsoft.Json.Linq
 
+    let private _jsonAdd(json:JObject, (key, value)) =
+        json.[key] <- JToken.FromObject(value)
+        json
+    let fromObj o = JsonConvert.SerializeObject (o:obj)
+    let createJObject pairs = JSON.JObject (Some pairs)
+    let rec private _jsonTokenize = function
+        | JString s -> JToken.op_Implicit s
+        | JNumber n -> if n = Decimal.Truncate n
+                           then JToken.op_Implicit (int n)
+                           else JToken.op_Implicit n
+        | JArray array -> new JArray(array
+                                     |> List.map (fun el -> _jsonTokenize el :> obj)
+                                     |> List.toArray
+                                    ) :> JToken
+        | JObject None -> JToken.FromObject null
+        | JObject (Some pairs) -> new JObject( pairs
+                                               |> List.map (fun (k,v) -> new JProperty(k, _jsonTokenize v) :> obj)
+                                               |> List.toArray
+                                             ) :> JToken
+    let fromKeyPairs pairs = 
+        let token = _jsonTokenize <| createJObject pairs
+        token.ToString(Formatting.None)
 
 let putAsync(url:string, data) =
     let json = new StringContent(data)
--- FsFirebaseTest/FsFirebaseTest.fs
+++ FsFirebaseTest/FsFirebaseTest.fs
@@ -1,13 +1,24 @@
-﻿module FsFirebaseTest
+﻿module FsFirebaseTest.Tests
 
 open NUnit.Framework
 open FsUnit
 open FsFirebase
 
-[<Test>]
-let ``When serializing a list of int into JSON`` () =
-    let data = [1;2;3;4;5]
+let [<Test>] ``When serializing a list of int into JSON`` () =
+    Json.fromObj [1;2;3;4;5] |> should equal "[1,2,3,4,5]"
 
-    let result = Json.fromObj data
+type SomeRecord = {key:string; value: obj}
+let [<Test>] ``Given a record, fromObj should return correct JSON`` () =
+    Json.fromObj {key="A"; value=99} |> should equal """{"key":"A","value":99}"""
 
-    result |> should equal "[1,2,3,4,5]"
+let [<Test>] ``Given a list of key pairs, fromKeyPairs should return a correct JSON text`` () =
+    Json.fromKeyPairs [ "key", JString "A"
+                        "value", JNumber 99m
+                      ]
+    |> should equal """{"key":"A","value":99}"""
+
+let [<Test>] ``Object with array value, the order should be preserved`` () =
+    Json.fromKeyPairs [ "key", JArray [JString "A"; JString "B"; JString "C"]
+                        "value", JArray [JNumber 1m; JNumber 2m; JNumber 3m]
+                      ]
+    |> should equal """{"key":["A","B","C"],"value":[1,2,3]}"""
