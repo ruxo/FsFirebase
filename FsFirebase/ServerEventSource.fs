c658079d75193b47e5ae682630b704a5a0bdff9e
Ruxoz <github@just.sent.at>
2014-10-13 21:26:55 +0700

    Test _interpretLine and _createNetworkStream

--- FsFirebase/ServerEventSource.fs
+++ FsFirebase/ServerEventSource.fs
@@ -22,7 +22,7 @@ type ConnectionResult =
 let private __contentType (response:HttpResponseMessage) = response.Content.Headers.ContentType
 let private __location (response:HttpResponseMessage) = response.Headers.Location
 
-let createEventSourceMessage (uri:Uri) lastEventId = 
+let _createEventSourceMessage (uri:Uri) lastEventId = 
     let message = new HttpRequestMessage(HttpMethod.Get, string uri)
     message.Headers.Accept.Add (MediaTypeWithQualityHeaderValue TextEventStreamMime)
     message.Headers.CacheControl <- CacheControlHeaderValue(NoCache=true)
@@ -30,7 +30,7 @@ let createEventSourceMessage (uri:Uri) lastEventId =
         then message.Headers.Add("Last-Event-ID", Option.get<string> lastEventId)
     message
 
-let connectEventSourceServer message =
+let _connectEventSourceServer message =
     async {
         use client = new HttpClient()
 
@@ -63,73 +63,78 @@ type EventSourceType =
     | Comment of string
     | Field of string * string
 
-let private _interpretLine (line:string) =
+let _interpretLine (line:string) =
     let normalized = line.Trim()
     match normalized.IndexOf(':') with
     | -1 when String.IsNullOrEmpty(normalized) -> BlankLine
     | -1 -> Field (normalized, String.Empty)
-    | 0 -> Comment <| normalized.Substring(1)
-    | n -> Field (normalized.Substring(1,n).TrimEnd(), normalized.Substring(n+1).TrimStart())
+    | 0 -> Comment <| normalized.Substring(1).TrimStart()
+    | n -> Field (normalized.Substring(0,n).TrimEnd(), normalized.Substring(n+1).TrimStart())
 
 type EventSourceMessage =
     | Comment of string list
     | ServerEvent of string * string list
 
-let private _createEventSourceStack =
-    Observable.byteToCharStream System.Text.Encoding.UTF8
-    >> Observable.charToLineStream
-    >> Observable.map _interpretLine
+let _createEventSourceStack  = Observable.byteToCharStream System.Text.Encoding.UTF8
+                               >> Observable.charToLineStream
+                               >> Observable.map _interpretLine
 
-type private ProcessorState =
+type ProcessorState =
     { Event:string
       Data: string list
       Comment: string list
     }
     static member empty = {Event=String.Empty; Data=[]; Comment=[]}
 
+let _processServerEvent (stream:ObservableSource<EventSourceMessage>, updateLastId, updateReconnectionTime) current es =
+    match es with
+    | Field (field, data) ->
+        match field with
+        | "event" -> { current with Event=data }
+        | "data" -> { current with Data=data::current.Data }
+        | "id" -> updateLastId <| if String.IsNullOrEmpty data then None else Some data
+                  current
+        | "retry" -> match Int32.TryParse data with
+                     | false, _ -> current
+                     | true, value -> updateReconnectionTime <| Milliseconds value
+                                      current
+        | _ -> current
+
+    | EventSourceType.Comment comment ->
+        { current with Comment=comment::current.Comment }
+
+    | BlankLine ->
+        if current.Comment <> []
+            then stream.Push <| EventSourceMessage.Comment (List.rev current.Comment)
+                 { current with Comment=[] }
+        elif current.Data <> []
+            then stream.Push <| ServerEvent (current.Event, List.rev current.Data)
+                 { current with Event=""; Data=[] }
+            else current
+
+let _createNetworkStream procParam =
+    let networkSink = ObservableSource.create()
+    networkSink
+    |> _createEventSourceStack
+    |> Observable.scan (_processServerEvent procParam) ProcessorState.empty
+    |> Observable.subscribe (fun _ -> ())
+    |> ignore
+    networkSink
+
+
 type EventSourceProcessor(?initReconnectionTime, ?initLastId) =
     let stream = ObservableSource.create()
     let mutable lastId: string option = initLastId
     let mutable reconnectionTime = defaultArg initReconnectionTime (Milliseconds 0)
 
-    let processServerEvent current es =
-        match es with
-        | Field (field, data) ->
-            match field with
-            | "event" -> { current with Event=data }
-            | "data" -> { current with Data=data::current.Data }
-            | "id" -> lastId <- if String.IsNullOrEmpty data then None else Some data
-                      current
-            | "retry" -> match Int32.TryParse data with
-                         | false, _ -> current
-                         | true, value -> reconnectionTime <- Milliseconds value
-                                          current
-            | _ -> current
-
-        | EventSourceType.Comment comment ->
-            { current with Comment=comment::current.Comment }
-
-        | BlankLine ->
-            if current.Comment <> []
-                then stream.Push <| EventSourceMessage.Comment (List.rev current.Comment)
-                     { current with Comment=[] }
-            elif current.Data <> []
-                then stream.Push <| ServerEvent (current.Event, List.rev current.Data)
-                     { current with Event=""; Data=[] }
-                else current
-
-    let networkSink = ObservableSource.create()
-    let processor = networkSink
-                    |> _createEventSourceStack
-                    |> Observable.scan processServerEvent ProcessorState.empty
-
     interface IObservable<EventSourceMessage> with
         member x.Subscribe observer = (stream :> IObservable<EventSourceMessage>).Subscribe observer
 
     member x.Start uri uriModifier =
-        let message = createEventSourceMessage uri lastId
+        let networkSink = _createNetworkStream (stream, (fun id -> lastId <- id), (fun time -> reconnectionTime <- time))
+        let message = _createEventSourceMessage uri lastId
         async {
-            let! response = connectEventSourceServer message
+            let! response = _connectEventSourceServer message
             match response with
             | OK networkStream ->
                 Observable.observeStream (Async.RunSynchronously) networkSink networkStream
---
+++ FsFirebaseTest/ServerEventSourceTest.fs
@@ -0,0 +1,31 @@
+﻿module ServerEventSourceTest
+
+open System.Collections.Generic
+open Xunit
+open FsUnit.Xunit
+open FsFirebaseUtils
+open ServerEventSource
+
+let [<Fact>] ``Check line interpretation algorithm``() =
+    ServerEventSource._interpretLine "" |> should equal BlankLine
+    ServerEventSource._interpretLine "event type" |> should equal (Field ("event type", ""))
+    ServerEventSource._interpretLine " : Comment!" |> should equal (EventSourceType.Comment "Comment!")
+    ServerEventSource._interpretLine "field: data" |> should equal (Field ("field", "data"))
+
+let [<Fact>] ``Fill network stream with an event 'greet' and data 'hello world!'``() =
+    let output = ObservableSource.create()
+    let ns = ServerEventSource._createNetworkStream(output, (fun _ -> ()), (fun _ -> ()))
+    let buffer = Queue()
+    output |> Observable.subscribe (fun e -> buffer.Enqueue e) |> ignore
+
+    let data = System.Text.Encoding.UTF8.GetBytes("""
+data: YHOO
+data: +2
+data: 10
+
+"""                                              )
+    data |> Array.iter (fun b -> ns.Push b) 
+
+    let result = Seq.toList buffer
+
+    result |> should equal [ ServerEvent ("", ["YHOO"; "+2"; "10"]) ]
