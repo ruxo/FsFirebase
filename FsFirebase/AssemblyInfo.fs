c6df963ecbda3ecad81d5a34db4a576cc449d20c
Ruxoz <github@just.sent.at>
2014-10-20 09:16:48 +0700

    Add assembly info and Nuget spec

---
+++ FsFirebase/AssemblyInfo.fs
@@ -0,0 +1,23 @@
+﻿module FsFirebase.AssemblyInfo
+
+open System
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("FsFirebase")>]
+[<assembly: AssemblyDescription("Firebase REST API wrapper for F#")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("RZ")>]
+[<assembly: AssemblyProduct("FsFirebase")>]
+[<assembly: AssemblyCopyright("Copyright ©  2014")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyDelaySign(false)>]
+
+[<assembly: CLSCompliant(false)>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("a3134f2f-31e0-4123-a148-2ab5d27595aa")>]
+
+()
